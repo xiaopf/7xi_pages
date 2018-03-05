ff3619a6d8d3a6c7d666e0dbe41572bc8d0ac08f
xiaopf <326258842@qq.com>
2018-03-05 18:49:52 +0800

    fixed bugs

--- script/script.js
+++ script/script.js
@@ -92,94 +92,80 @@ function boy_move(){
 
 // 太阳和云朵运动函数
 function bg_sun_cloud(){
-	var dfd=$.Deferred();
-	$sun.animate({
+	$sun.stop().animate({
 	    'left':-$sun.width(),
 	    'top':$sun.height()*1.2
 	},5000,'linear');
-	$cloud1.animate({
+	$cloud1.stop().animate({
 	    'left':'100%'
 	},12000,'linear');
-    $cloud2.animate({
+    $cloud2.stop().animate({
 	    'left':'0%'
 	},12000,'linear');
 }
 
 // 男孩开始走路，速度是时间控制，pos 是到达位置
 function walk(speed,pos){
-    var dfd=$.Deferred();
+
     
-	$boy.animate({
+	$boy.stop().animate({
 	    'left':cWidth*pos  
-	},speed,'linear',function(){
-		dfd.resolve();
-	});
-	return dfd.promise();
+	},speed,'linear')
 }
 
 //页面整体滑动 
 function scroll(speed,dis){
-	var dfd=$.Deferred();
-	$ul.animate({
+	$ul.stop().animate({
 		'left':-dis
-	},speed,'linear',function(){
-         dfd.resolve();
-	});
-	return dfd;
+	},speed,'linear');
+
+
+
 }
 
 // 商店门开
 function open_door(){
-	var dfd=$.Deferred();
-	door_left.animate({
+	door_left.stop().animate({
 		'left':'-50%'
-	},500,'linear',function(){
-         dfd.resolve();
-	});
-    door_right.animate({
+	},500,'linear');
+    door_right.stop().animate({
 		'left':'100%'
-	},500,'linear',function(){
-         dfd.resolve();
-	});
-	return dfd.promise();
+	},500,'linear');
 }
 
 // 天空中的飞鸟
 function bird_fly(){
-	$bird.animate({
+	$bird.stop().animate({
 	    'left':'-100%' 
 	},20000,'linear')
 }
 
 function walk_in(){
-	var dfd=$.Deferred();
 	// 男孩向里走，透明度和大小的变化
     $boy.addClass('walk_in1').addClass('walk_in2');
     // 男孩的坐标变化
-	$boy.animate({
+	$boy.stop().animate({
        'left':cWidth+door.position().left+door.width()/2-$boy.width()/2, 
 	},100,'linear')
 
 
-	setTimeout(function(){
-		dfd.resolve();
-	},1800);
-	return dfd.promise();
 
 }
 
 
 // 男孩从商店走出
 function walk_out(){
-    var dfd=$.Deferred();
 
+// 
     $boy.removeClass('walk_slow').removeClass('walk_in1');
     $boy.addClass('boy_flower').addClass('walk_slow_flower').addClass('walk_in3');
+    
+    setTimeout(function(){
+    	$boy.removeClass('walk_in2');
+    },2000)
+    
 
-    setTimeout(function(){ 	
-      	dfd.resolve();  
-    },1800);
-    return dfd.promise();
+
 }
 
 
@@ -205,69 +191,55 @@ function light_dark(){
 
 // 商店延迟2s后关门
 function close_door(){
-	var dfd=$.Deferred();
 
-    door_left.animate({
+    door_left.stop().animate({
 			'left':'0%'
 		},500,'linear');
-	door_right.animate({
+	door_right.stop().animate({
 			'left':'50%'
 		},500,'linear');
 
-    setTimeout(function(){
-	    dfd.resolve();	
-	},1200);	 
-	return dfd.promise();
+
 }
 
 
 
 function walk_up(speed){
-    var dfd=$.Deferred();
-	$boy.animate({
+
+	$boy.stop().animate({
 	    'left':2*cWidth+girlLeft-$boy.width()*0.87,
 	    'top':$girl.offset().top-(boyHeight-0.99*girlHeight) 
-	},speed,'linear',function(){
-		dfd.resolve();
-	});
-	return dfd.promise();
+	},speed,'linear');
+
 }
 
 
 function change_cha(){
-	var dfd=$.Deferred();
+	stop_walk();
 	setTimeout(function(){
-		stop_walk();
-		$boy.removeClass('walk_slow_flower').removeClass('walk_in2').removeClass('walk_in3');
+		
+		$boy.removeClass('walk_slow_flower').removeClass('walk_in3');
 	    $boy.addClass('boy_flower_last');
 	    $girl.addClass('girl_last');
 	    
-	},3400);
+	},500);
 
-	setTimeout(function(){
-	    dfd.resolve();	
-	},3500);	 
-	return dfd.promise();
+
 }
 
 
 function turn_around(){
-	var dfd=$.Deferred();
 
 	setTimeout(function(){
 	    $boy.addClass('boy_turn_around');
 		$girl.addClass('girl_turn_around');
-		$boy.animate({
+		$boy.stop().animate({
 		    'left':2*cWidth+girlLeft-$boy.width()*0.788,
 		    'top':$girl.offset().top-(boyHeight-0.991*girlHeight)  
 		},50,'linear');
 	},1200);
     
-    setTimeout(function(){
-	    dfd.resolve();	
-	},1300);
 
-	return dfd.promise();
 }
 
 
@@ -277,12 +249,18 @@ function show_slogen(){
     $('#music1').get(0).pause();
 }
 
+// $('.pages').css({
+
+//     'transform': 'translateX(-3840px)',
+// })
+
+// creat_flower();
 
 function creat_flower(){
-	var dfd=$.Deferred();
 	var arr_flower=['images/snowflake/snowflake1.png','images/snowflake/snowflake2.png','images/snowflake/snowflake3.png','images/snowflake/snowflake4.png','images/snowflake/snowflake5.png','images/snowflake/snowflake6.png'];
 	var arr_ratateSpeed=[10,15,20,22,30,35,33,18,13,9,25,27];
-	setInterval(function(){
+	clearInterval(timer);
+	var timer = setInterval(function(){
 	    var getPic=arr_flower[Math.floor(Math.random()*6 )];
 	    var getSpeed=arr_ratateSpeed[Math.floor(Math.random()*12)]
 		var getLeft=Math.floor(Math.random()*140-30 );
@@ -302,16 +280,24 @@ function creat_flower(){
 				    '-webkit-animation':''+getSpeed+'s snow_flower_rotate infinite',
 				    '-moz-animation':''+getSpeed+'s snow_flower_rotate infinite',
 				    'animation':''+getSpeed+'s snow_flower_rotate infinite'
-		}).animate({
+		}).stop().animate({
 			'top' :'100%',
 			'left':getLeft+getLeftOffset+'%'
 		},7000,'linear',function(){
 			$(this).remove();
 		});
         $snow_flower.append(newFlower);
-        dfd.resolve();
+ 
+        $('.snow_flower_rotate').each(function(){
+        	if(parseInt($(this).css("top")) >= cHeight){
+        		$(this).remove();
+        	}
+        });
+
+        console.log($('.snow_flower_rotate').length);
+
 	},40);
-	return dfd.promise();
+
 }
 
 // 
@@ -327,50 +313,112 @@ function creat_flower(){
 // });
 
 
-//动画载入延时1s 执行 给预加载留出时间
+// 动画载入延时1s 执行 给预加载留出时间
 setTimeout(start,1000);
 
 
+
+  
+
 function start(){
+    let promise = function(time,num){
+      	return  new Promise(function(resolve,reject){
 
-    init_music();
-	boy_move(); 
-    bg_sun_cloud();
+      		if(num === 1){
+      			init_music();
+      			boy_move(); 
+      			bg_sun_cloud();
+      			walk(4500,0.6);
+      		}
 
-	$.when(walk(4500,0.6)).then(function(){
-		walk(7100,1.5);
-		return scroll(7100,cWidth);
-	}).then(function(){
-		stop_walk();
-		bird_fly();
-		return open_door();
-	}).then(function(){	
-        light_bright();
+
+   		   	  setTimeout(function(){
+   		   	  	 resolve()
+   		   	  },time)
+   		   	})
+      }
+
+
+    promise(4500,1)
+    .then(function(value){
+
+  		walk(7100,1.5);
+  		scroll(7100,cWidth);
+      	return promise(7300,2)
+
+    })
+
+    .then(function(value){
+
+      	stop_walk();
+      	bird_fly();
+      	open_door();
+      	light_bright();
+      	return promise(500,3)
+
+    })
+    .then(function(value){
+  	        
 		re_walk();
-		return walk_in()      //动画有时间，但是添加属性一下就搞定了
-	}).then(function(){
-		return walk_out();    
-	}).then(function(){
-		return close_door();  
-	}).then(function(){
-		light_dark();
-		walk(5100,2.2);
-		return scroll(7100,2*cWidth);
-	}).then(function(){
-		walk_up(1350);
-	    return  change_cha();
-	}).then(function(){
-		return turn_around();  
-    }).then(function(){
-    	show_slogen();
-		return creat_flower();  
+		walk_in() 
+
+      	return promise(2000,4)
+    })
+
+    .then(function(value){
+        	        
+      	walk_out()
+    	return promise(1000,5)
+
+    })
+
+    .then(function(value){
+          	        
+    	close_door();
+    	light_dark();
+
+      	return promise(1000,6)
     })
 
 
-}
-   
+    .then(function(value){
+        	        
+      	walk(7100,2.2)
+      	scroll(7100,2*cWidth);
 
+    	return promise(7500,7)
+    })
 
+    .then(function(value){
+
+    	walk_up(2500)
+    	return promise(2400,8)
+    })
+
+    .then(function(value){
+
+    	change_cha()
+    	
+    	return promise(1000,8)
+    })
+
+    .then(function(value){
+
+    	turn_around();
+    	return promise(2000,8)
+    })
+
+    .then(function(value){
+
+    	show_slogen();
+    	return promise(1000,8)
+    })
+
+    .then(function(value){
+        creat_flower()
+     })
+
+  }
 
 
 
